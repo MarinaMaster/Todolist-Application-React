a7f28daee05295273927d666d1cdf74e3ca726bf
Marina Myssak <marina.myssak@outlook.com>
2024-01-23 07:20:16 +0600

    complite task

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue]= useState("")
+	const [todos, setTodos] = useState([])
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-sm">
+			<div className="text-center">todos</div>
+			<input
+				onChange={(event) => setInputValue(event.target.value)}
+				value={inputValue}
+				onKeyPress={(event) => {
+					if (event.key === "Enter" && inputValue.trim()) {
+						setTodos(todos.concat(inputValue));
+						setInputValue("");
+					}
+				}}
+				type="text"
+				className="form-control"
+				aria-describedby="inputGroup-sizing-default"
+				placeholder={"What needs to be done?"}>
+			</input>
+			<ol className="list-group">
+				{todos.map((item, index) => (
+					<li className="list-group-item d-flex justify-content-between align-items-start" key={index}>
+						<div className="ms-2 me-auto">
+							<div className="fw-bold">{item}</div>
+						</div>
+						<button
+							className="btn"
+							onClick={() =>
+								setTodos(todos.filter((t, currentIndex) => currentIndex !== index))
+							}
+							type="submit">X</button>
+					</li>
+				))}
+			</ol>
 		</div>
 	);
 };
